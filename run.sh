0ee803a1880c354593085694495b26b399a33c99
Orabig <benoit.chauvet@gmail.com>
2016-09-16 08:14:40 +0200

    Changed run and build scripts

--- run.sh
+++ run.sh
@@ -2,9 +2,7 @@
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
-docker build -t dropzone .
-
 docker rm -f dropzonedemo
-docker run --name=dropzonedemo -d -p 9181:80 \
+docker run --restart=always --name=dropzonedemo -d -p 9181:80 \
     -v $DIR/uploads:/var/www/uploads \
     dropzone
